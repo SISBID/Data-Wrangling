8bc7706d9d61df7e33a540b840107456beb1e883
muschellij2 <muschellij2@gmail.com>
2020-07-09 00:07:22 -0400

    making the html

--- run_lectures.R
+++ run_lectures.R
@@ -1,8 +1,9 @@
+library(pagedown)
 x = list.files(pattern = ".Rmd$",
                path = here::here("lecture_notes"), 
                full.names = TRUE,
                recursive = TRUE)
-
+x = x[!grepl("Bioconductor_intro.Rmd", x)]
 sapply(x, function(x) {
   out = sub(".Rmd$", ".R", x)
   knitr::purl(input = x, output = out)
@@ -10,3 +11,5 @@ sapply(x, function(x) {
 
 sapply(x, rmarkdown::render, envir = new.env())
 
+xx = sub("Rmd$", "html", x)
+sapply(xx, pagedown::chrome_print)
