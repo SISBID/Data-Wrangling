34e690b561897cd12c1380911156cc000a1f0a81
AM Hoffman <avamariehoffman@gmail.com>
2021-07-13 06:08:14 -0700

    Add more challenging question to joining lab

--- labs/merging-lab-key.R
+++ labs/merging-lab-key.R
@@ -27,3 +27,24 @@ nrow(left)
 right = right_join(road, crash)
 nrow(right)
 
+
+## -----------------------------------------------------------------------------
+no_dat = crash %>% 
+  anti_join(road) %>% 
+  group_by(Road) %>%
+  summarize(total_crashes = sum(N_Crashes))
+no_dat
+
+
+## -----------------------------------------------------------------------------
+corrections = read_csv("https://sisbid.github.io/Data-Wrangling/labs/crashes_corrections.csv")
+
+corrections$ROAD = corrections$ROAD %>% str_replace("INTERSTATE", "Interstate")
+
+crash %>% 
+  full_join(corrections, by = c("Year" = "YEAR", "Road" = "ROAD"), suffix = c(".orig", ".checked")) %>%
+  mutate(Corrected = ifelse(is.na(N_Crashes.checked), 0, 1)) %>%
+  mutate(N_Crashes = ifelse(Corrected == 0, N_Crashes.orig, N_Crashes.checked)) %>%
+  select(Year, Road, N_Crashes, Volume.orig, Corrected) %>%
+  rename(Volume = Volume.orig)
+
