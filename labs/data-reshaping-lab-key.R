e975d6dc46492f641980f6d4d02410f5e9629555
avahoffman <avamariehoffman@gmail.com>
2022-07-25 22:37:21 -0600

    update lab

--- labs/data-reshaping-lab-key.R
+++ labs/data-reshaping-lab-key.R
@@ -1,55 +1,46 @@
-## ----setup, include=FALSE-----------------------------------------------------
+## ----setup, include=FALSE----------------------------------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
 
-## -----------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------
 library(tidyverse)
 
 
-## ---- message = FALSE---------------------------------------------------------
-wide = read_csv(
-  "http://sisbid.github.io/Data-Wrangling/data/Charm_City_Circulator_Ridership.csv"
-)
+## ---- message = FALSE--------------------------------------------------------------------------------------------------
+wide <- read_csv("http://sisbid.github.io/Data-Wrangling/data/Charm_City_Circulator_Ridership.csv")
 
 
-## -----------------------------------------------------------------------------
-long = wide %>% 
-  pivot_longer( !c(daily, day, date), names_to = "variable", values_to = "n_people")
+## ----------------------------------------------------------------------------------------------------------------------
+long <- wide %>% 
+  pivot_longer( !c(daily, day, date), names_to = "name", values_to = "n_people")
 head(long)
 
 
-## -----------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------
 # We over-wrote the day column by assigning it new values (in this case, calendar "day")
-long2 = long %>%
+long2 <- long %>%
   separate(col = "date", into = c("month", "day", "year"),
            sep = "/", remove = FALSE)
 table(long2$day)
 
 
-## -----------------------------------------------------------------------------
-long2 = long2 %>%
-  unite(col = "newdate", year, month, day, sep = "-")
-head(long2)
-
-
-## ---- error = TRUE------------------------------------------------------------
-long = long %>% 
+## ---- error = TRUE-----------------------------------------------------------------------------------------------------
+long3 <- long %>%
   mutate(
-    variable = variable %>% 
-      str_replace("Board", "_Board") %>% 
-      str_replace("Alight", "_Alight") %>% 
-      str_replace("Average", "_Average") 
-  )
-head(long)
+    name = str_replace(name, "Board", "_Board"),
+    name = str_replace(name, "Alight", "_Alight"),
+    name = str_replace(name, "Average", "_Average")
+  ) %>%
+  separate(name, into = c("route", "type"), sep = "_")
 
 
-## -----------------------------------------------------------------------------
-long = long %>%
-  separate(col = "variable", into = c("route", "type"), sep = "_")
-head(long)
 
-
-## -----------------------------------------------------------------------------
-wide_by_route = long %>% 
+## ----------------------------------------------------------------------------------------------------------------------
+wide_by_route <- long3 %>% 
   pivot_wider(names_from = route, values_from = n_people)
+
+
+## ----------------------------------------------------------------------------------------------------------------------
+wide_by_day <- long3 %>% 
+  pivot_wider(names_from = day, values_from = n_people)
 
