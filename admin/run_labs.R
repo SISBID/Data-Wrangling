f74534a98d0ce3caafd2426d6684f8b26c1fae46
avahoffman <avamariehoffman@gmail.com>
2026-07-20 13:47:48 -0400

    Don't make R files in the Lab render

--- admin/run_labs.R
+++ admin/run_labs.R
@@ -5,22 +5,26 @@ x <- list.files(
   recursive = TRUE
 )
 
+make_R_files <- FALSE # Should .R equivalents of .Rmd files be made?
+
 # Keep only lab files
 x <- x[!grepl("index|archive|admin", x)]
 x <- x[grepl("lab", x)]
 x <- x[grepl("key", x)] # Only render keys
 
 # Make .R file if .Rmd file has been changed
-cli::cli_alert(cli::col_cyan("Making .R files..."))
-invisible(sapply(x, function(x) {
-  x_r <- sub(".Rmd$", ".R", x)
-  if (!file.exists(x_r) ||
-      file.info(x_r)$mtime <
-      file.info(x)$mtime) {
-    knitr::purl(input = x, output = x_r)
-  }
-}))
-cli::cli_alert(cli::col_cyan(".R files made!"))
+if (make_R_files) {
+  cli::cli_alert(cli::col_cyan("Making .R files..."))
+  invisible(sapply(x, function(x) {
+    x_r <- sub(".Rmd$", ".R", x)
+    if (!file.exists(x_r) ||
+        file.info(x_r)$mtime <
+        file.info(x)$mtime) {
+      knitr::purl(input = x, output = x_r)
+    }
+  }))
+  cli::cli_alert(cli::col_cyan(".R files made!"))
+}
 
 # Make .html file if .Rmd file has been changed
 cli::cli_alert(cli::col_cyan("Making HTMLs..."))
